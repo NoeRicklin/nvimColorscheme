27709eb6a7e348a59facdd317314c2571eb6b29a
NoeRicklin <noe.ricklin@outlook.de>
2025-04-07 10:17:21 +0200

    Added more colors

--- colors/fortune.lua
+++ colors/fortune.lua
@@ -1,2 +1,1 @@
 require("fortune").load()
-print("required")
--- lua/fortune/palette.lua
+++ lua/fortune/palette.lua
@@ -1,14 +1,15 @@
 local colors = {
-	Keyword		= { fg = "#16161D" },
-	Statement	= { fg = "#128400" },
-	LineNr		= { fg = "#3d6b2c" },
-	Normal		= { fg = "#fff8c7" },
+	Keyword		= { fg = "#4e5c46" },
+	Statement	= { fg = "#91843c" },
+	LineNr		= { fg = "#8bba56" },
+	Normal		= { fg = "#ded6a9" },
 	Cursor		= { bg = "#fff8c7" },
 	Comment		= { fg = "#73825a" },
 	Visual		= { bg = "#3c4d38" },
 	ErrorMsg	= { bg = "#9c301f" },
 	Statusline	= { bg = "#7c8a43",
 					fg = "#382703" },
+	Type		= { fg = "#71a13b" },
 }
 
 --dd 
