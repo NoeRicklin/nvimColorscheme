65468c8439c44996dc77ddfcda117f597b06ec6d
NoeRicklin <noe.ricklin@outlook.de>
2025-04-07 09:29:55 +0200

    Added more colors

--- lua/fortune/palette.lua
+++ lua/fortune/palette.lua
@@ -1,6 +1,15 @@
 local colors = {
-	Keyword = { fg = "#16161D" },
-	Statement = { fg = "#128400" }
+	Keyword		= { fg = "#16161D" },
+	Statement	= { fg = "#128400" },
+	LineNr		= { fg = "#3d6b2c" },
+	Normal		= { fg = "#fff8c7" },
+	Cursor		= { bg = "#fff8c7" },
+	Comment		= { fg = "#73825a" },
+	Visual		= { bg = "#3c4d38" },
+	ErrorMsg	= { bg = "#9c301f" },
+	Statusline	= { bg = "#7c8a43",
+					fg = "#382703" },
 }
 
+--dd 
 return colors
